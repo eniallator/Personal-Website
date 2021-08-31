fb2986fdc3e875e150e5591a73a619e93846dbc6
eniallator <eniallatormc@gmail.com>
2021-08-31 10:57:33 +0100

    Fixed age at bottom of the resume and changed the startup print message

--- index.js
+++ index.js
@@ -45,4 +45,6 @@ app.post("/", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () =>
+  console.log(`Personal website listening on port ${port}!`)
+);
