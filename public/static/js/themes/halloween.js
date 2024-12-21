b6b1fc6bce7e289e3b617bcdfa0a236a0e1c17a5
eniallator <eniallatormc@gmail.com>
2024-12-21 12:36:53 +0000

    Fixed halloween animation

--- public/static/js/themes/halloween.js
+++ public/static/js/themes/halloween.js
@@ -1,75 +1,70 @@
-let lastScrolledAt = Date.now();
-const eyesDelay = 5000;
+const spawnDelay = 5000;
+const spawnInterval = 1000;
+const halfFadeTime = 3000;
 
-window.onscroll = function () {
-  document
-    .querySelectorAll("#theme-container > img")
-    .forEach((img) => img.remove());
-  document.getElementById("theme-overlay").classList.remove("fadein");
-  lastScrolledAt = Date.now();
+let lastScrolled = Date.now();
+let eyesId = 0;
+let activeEyes = [];
+
+const container = document.querySelector("#theme-container");
+const overlay = document.getElementById("theme-overlay");
+
+const eyesLoop = () => {
+  const now = Date.now();
+  if (lastScrolled + spawnDelay > now) return;
+
+  if (!overlay.classList.contains("fadein")) {
+    document.getElementById("theme-overlay").classList.add("fadein");
+  }
+
+  activeEyes = activeEyes.filter(({ id, spawnTime }) => {
+    const el = document.getElementById(`theme-eyes-${id}`);
+    if (spawnTime + 2 * halfFadeTime < now) {
+      el.remove();
+      return false;
+    } else {
+      el.style.opacity = `${Math.round((1 - Math.abs(now - spawnTime - halfFadeTime) / halfFadeTime) * 100)}%`;
+      return true;
+    }
+  });
+
+  if (activeEyes[0] == null || activeEyes[0].spawnTime < now - spawnInterval) {
+    const bounds = container.getBoundingClientRect();
+    const scale = 0.4 + Math.random() * 0.6;
+    const width = Math.round(scale * 109);
+    const height = Math.round(scale * 30);
+
+    const eyes = { id: eyesId++, spawnTime: now };
+
+    activeEyes.unshift(eyes);
+
+    container.innerHTML += `
+        <img
+          id="theme-eyes-${eyes.id}"
+          src='static/images/themes/spooky-eyes.png'
+          width="${width}"
+          height="${height}"
+          style="
+            top: ${Math.round(Math.random() * (bounds.height - height))}px;
+            left: ${Math.round(Math.random() * (bounds.width - width))}px;
+            opacity: 0%;
+          "
+        />`;
+  }
+  requestAnimationFrame(eyesLoop);
 };
 
-const activeEyes = [];
-let eyesId = 0;
-const eyesFadeOutTime = 3000;
+let eyesTimeout = setTimeout(eyesLoop, spawnDelay);
 
-function spawnEyes() {
-  if (lastScrolledAt + eyesDelay < Date.now()) {
-    activeEyes.push({
-      id: eyesId,
-      createdAt: Date.now(),
-      lifeTime: 10000 + Math.random() * 2000,
-    });
+window.onscroll = function () {
+  container.innerHTML = "";
+  overlay.classList.remove("fadein");
+  lastScrolled = Date.now();
+  eyesId = 0;
+  activeEyes = [];
 
-    const scale = 0.4 + Math.random() * 0.6;
-
-    document.getElementById("theme-container").innerHTML +=
-      "<img width='" +
-      Math.round(scale * 109) +
-      "px' height='" +
-      Math.round(scale * 30) +
-      "px' id='eyes-" +
-      eyesId +
-      "' src='static/images/themes/spooky-eyes.png' style='top: " +
-      Math.round(Math.random() * 100) +
-      "%; left: " +
-      Math.round(Math.random() * 100) +
-      "%'/>";
-
-    eyesId = (eyesId + 1) % 10;
+  if (eyesTimeout != null) {
+    clearTimeout(eyesTimeout);
   }
-
-  setTimeout(spawnEyes, 2000 + Math.random() * 500);
-}
-
-function eyesCleanUp() {
-  if (lastScrolledAt + eyesDelay < Date.now()) {
-    document.getElementById("theme-overlay").classList.add("fadein");
-    const currTime = Date.now();
-    for (let i = activeEyes.length - 1; i >= 0; i--) {
-      const eyes = activeEyes[i];
-      const eyesEl = document.querySelector(
-        "#theme-container > #eyes-" + eyes.id
-      );
-      if (eyes.createdAt + eyes.lifeTime < currTime) {
-        eyesEl.remove();
-        activeEyes.splice(i, 1);
-      }
-      if (
-        !eyesEl.classList.contains("fadeout") &&
-        eyes.createdAt + eyes.lifeTime - eyesFadeOutTime <= currTime
-      ) {
-        eyesEl.classList.add("fadeout");
-      }
-    }
-  }
-
-  setTimeout(eyesCleanUp, 500);
-}
-
-function initEyes() {
-  spawnEyes();
-  eyesCleanUp();
-}
-
-setTimeout(initEyes, 5000);
+  eyesTimeout = setTimeout(eyesLoop, spawnDelay);
+};
