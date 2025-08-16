74492a94c730233ad9147099e7c2fef15b955014
eniallator <eniallatormc@gmail.com>
2025-08-16 10:49:51 +0100

    Added docker things

--- server/env.ts
+++ server/env.ts
@@ -9,11 +9,10 @@ const getEnvOrRaise = (name: string) =>
   getEnv(name) ?? raise(new Error(`${name} environment variable not found`));
 
 export const env = {
-  nodeEnv: getEnv("NODE_ENV"),
+  nodeEnv: getEnv("NODE_ENV") ?? "development",
   port: getEnv("PORT") ?? 3000,
   fullHost: getEnvOrRaise("FULL_HOST"),
   emailSender: getEnvOrRaise("EMAIL_SENDER"),
   emailSenderPass: getEnvOrRaise("EMAIL_SENDER_PASS"),
   emailRecipient: getEnvOrRaise("EMAIL_RECIPIENT"),
-  sendgridApiKey: getEnvOrRaise("SENDGRID_API_KEY"),
 };
